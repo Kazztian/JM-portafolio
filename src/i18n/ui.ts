d13c37aba6abaf3fdeb0169de03dacd6280fc091
Kazztian <juansmadridgalindo@gmail.com>
2026-04-21 21:59:22 -0500

    Commit - cambio de idioma LISTO

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: "English",
+    es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const showDefaultLang = false;
+
+export const ui = {
+  es: {
+    "nav.home": "Inicio",
+    "nav.projects": "Proyectos",
+    "nav.blog": "Blog",
+    "nav.about": "Acerca de",
+    "nav.contact": "Contacto",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.projects": "Projects",
+    "nav.blog": "Blog",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+  },
+};
